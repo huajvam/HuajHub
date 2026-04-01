f0584d9694319893fba46362b3fd90fab80edd72
HuajHub <kev.vue249@gmail.com>
2026-04-01 18:28:16 -0500

    Update registry.lua

--- src/core/registry.lua
+++ src/core/registry.lua
@@ -1,12 +1,10 @@
 return {
 	Places = {
-		-- Replace these with real PlaceIds
-		 [13901460084] = "mashle_academy",
+		-- [1234567890] = "mashle_academy",
 		-- [9876543210] = "deepwoken",
 	},
 
 	GameIds = {
-		-- Optional fallback mappings by GameId
 		-- [1111111111] = "mashle_academy",
 	},
 
