4ee5f980ece54fbb642e24e12db6204f4bef8212
HuajHub <kev.vue249@gmail.com>
2026-04-03 06:55:45 -0500

    Add files via upload

--- games/MashleAcademy.lua
+++ games/MashleAcademy.lua
@@ -3582,6 +3582,15 @@ local function setupAutoParryTab()
 		saveAutoParryMakerConfigsToFile()
 		Library:Notify("Deleted Auto Parry config.", 2)
 	end)
+	autoParryMakerGroup:AddButton("Refresh Configs", function()
+		autoParryMakerConfigs.Players = {}
+		autoParryMakerConfigs.Mobs = {}
+		autoParryState.currentBuilderConfigId = nil
+		loadAutoParryMakerConfigsFromFile()
+		syncAutoParryBuilderConfigsToRuntime()
+		refreshSavedConfigDropdown("(none)")
+		Library:Notify("Refreshed Auto Parry Maker configs.", 2)
+	end)
 	autoParryMakerGroup:AddButton("Attempt Automatic Get", function()
 		if onAutoGetMakerConfig then
 			onAutoGetMakerConfig()
